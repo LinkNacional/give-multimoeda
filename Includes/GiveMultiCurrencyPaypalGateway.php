f5c4fa3b72b465c91ba51219d1e58d55275acdfa
Euzebio Batista <euzebio.batista2@gmail.com>
2025-03-13 14:36:07 +0000

    fix: paypal script

---
+++ Includes/GiveMultiCurrencyPaypalGateway.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lkn\GiveMultimoedas\Includes;
+
+use Give\Helpers\Language;
+use Give\PaymentGateways\Gateways\PayPalCommerce\PayPalCommerceGateway;
+
+class GiveMultiCurrencyPaypalGateway extends PayPalCommerceGateway
+{
+    public function enqueueScript(int $formId)
+    {
+        $defaultCurrency = give_get_option('currency');
+        wp_enqueue_script('paypal-commerce', GIVE_MULTI_CURRENCY_URL . 'resource/payPalCommerceGateway.js', array('react', 'wp-components', 'wp-i18n'), GIVE_MULTI_CURRENCY_VERSION, true);
+        wp_localize_script('paypal-commerce', 'giveMulti', [
+            'currency' => $defaultCurrency,
+        ]);
+        Language::setScriptTranslations('paypal');
+    }
+}
